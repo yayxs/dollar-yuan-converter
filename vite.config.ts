5c05071bee1c70521b2b0c047a53006cbfd805be
yayxs <leaderywl@163.com>
2024-09-06 14:21:08 +0800

    feat: ssr init

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
-import mdx from "@mdx-js/rollup";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+import mdx from "@mdx-js/rollup";
+
 const viteConfig = defineConfig({
-	plugins: [mdx(/* jsxImportSource: …, otherOptions… */)],
+	plugins: [
+		{
+			enforce: "pre",
+			...mdx({
+				/* jsxImportSource: …, otherOptions… */
+			}),
+		},
+		react({ include: /\.(jsx|js|mdx|md|tsx|ts)$/ }),
+	],
 });
 
 export default viteConfig;
